30b474722b60d7470062dd519224fc460c4d35a1
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2024-12-16 22:09:49 +0300

    register ended

--- controller/userController.js
+++ controller/userController.js
@@ -3,27 +3,38 @@
          // db connection
 const dbconnection = require('../db/dbConfig')
 
+const  bcrypt = require('bcrypt')
+const  {StatusCodes} = require('http-status-codes')
 
 async function register(req,res) {
     // res.send("register")
     const {username, firstname, lastname, email,password } = req.body
     if (!username || !firstname || !lastname || !email || !password ){
-        return  res.status(400).json({msg: "please provide all required information"})
+        return  res.status(StatusCodes.BAD_REQUEST).json({ error: "Bad Request",
+                         msg: "please provide all required information"})
     }
     try {
         const [user] = await dbconnection.query ("SELECT username, userid FROM users WHERE username = ? or email = ?",[username,email])
         // return  res.json({user: user})
         if(user.length>0){
-            return  res.status(400).json({msg: "user already register"})
+            return  res.status(StatusCodes.CONFLICT).json({ error: "Conflict",
+                                         msg: "user already register"})
         }
         if(password.length <= 8){
-            return  res.status(400).json({msg: "password must be at least 8 characters"})
+            return  res.status(StatusCodes.BAD_REQUEST).json({ error: "Bad Request",
+                                    msg: "password must be at least 8 characters"})                        
         }
-        await dbconnection.query("INSERT INTO users (username,firstname ,lastname,email, password)VALUES (?,?,?,?,?)" ,[username,firstname ,lastname,email, password] )
-        return  res.status(201).json({msg: "user created"})
+
+        //   encrypt thev password
+        const salt = await bcrypt.genSalt(10)
+       const hashedpassword = await bcrypt.hash(password,salt) 
+        await dbconnection.query("INSERT INTO users (username,firstname ,lastname,email, password)VALUES (?,?,?,?,?)" ,[username,firstname ,lastname,email,  hashedpassword] )
+        return  res.status(StatusCodes.CREATED).json({  error: "Created",
+            msg: "User registered successfully"})
     } catch (error) {
         console.log(error.message);
-        return  res.status(500).json({msg: "something went wrong, try again later"})
+        return  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error: "Internal Server Error",
+                                 msg: "An unexpected error occurred."})
     }
 }
 
